464a7fdfb714ecb748051272886bda238dd54a3f
andriihrechko <andriihrechko250703@gmail.com>
2026-04-11 00:47:44 +0300

    feat(classes): add Cafe class

--- app/main.py
+++ app/main.py
@@ -2,3 +2,8 @@ import datetime
 
 from exceptions import (NotVaccinatedError, NotWearingMaskError,
                         OutdatedVaccineError)
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
