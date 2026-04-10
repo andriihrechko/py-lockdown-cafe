9d3c0abafa196e989cc5c4283635c72f7ba2c2ec
andriihrechko <andriihrechko250703@gmail.com>
2026-04-11 00:48:49 +0300

    feat(cafe): add visit_cafe method

--- app/main.py
+++ app/main.py
@@ -7,3 +7,14 @@ from exceptions import (NotVaccinatedError, NotWearingMaskError,
 class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError
+        if "expiration_date" in visitor["vaccine"]:
+            expiration_date = visitor["vaccine"]["expiration_date"]
+            if expiration_date < datetime.date.today():
+                raise OutdatedVaccineError
+        if "wearing_a_mask" in visitor and not visitor["wearing_a_mask"]:
+            raise NotWearingMaskError
+        return f"Welcome to {self.name}"
