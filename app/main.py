0769a4325f089f69b9555b4a33eb57aa34a32f46
andriihrechko <andriihrechko250703@gmail.com>
2026-04-11 01:23:28 +0300

    refactor(cafe): add new cafe file and import in main

--- app/main.py
+++ app/main.py
@@ -1,23 +1,6 @@
-import datetime
+from errors import NotWearingMaskError, VaccineError
 
-from errors import (NotVaccinatedError, NotWearingMaskError,
-                    OutdatedVaccineError, VaccineError)
-
-
-class Cafe:
-    def __init__(self, name: str) -> None:
-        self.name = name
-
-    def visit_cafe(self, visitor: dict) -> str:
-        if "vaccine" not in visitor:
-            raise NotVaccinatedError
-        if "expiration_date" in visitor["vaccine"]:
-            expiration_date = visitor["vaccine"]["expiration_date"]
-            if expiration_date < datetime.date.today():
-                raise OutdatedVaccineError
-        if "wearing_a_mask" in visitor and not visitor["wearing_a_mask"]:
-            raise NotWearingMaskError
-        return f"Welcome to {self.name}"
+from cafe import Cafe
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
